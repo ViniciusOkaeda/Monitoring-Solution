f1a3d8030ac0825bf3de1067d1ac01c719322ed3
ViniciusOkaeda <61584826+ViniciusOkaeda@users.noreply.github.com>
2022-03-31 17:25:39 -0300

    watching charts sendo inseridos

--- src/pages/watching/index.js
+++ src/pages/watching/index.js
@@ -9,8 +9,8 @@ import ThemeMenu from '../../components/themeMenu';
 import DrawerComponent from '../../components/drawer';
 import Header from '../../components/header/index.js';
 import PermanentDrawerLeft from '../../components/drawer';
-import Chart from '../../components/charts/PieChart.tsx';
-import SimpleChart from '../../components/charts/SimpleChart.tsx';
+
+import { Chart } from "react-google-charts";
 
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
@@ -27,36 +27,76 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-creative'
 
-import { PieChart, Pie, Legend, Tooltip } from "recharts";
+import { PieChart, Pie, Legend, Tooltip, Sector, Cell, BarChart, XAxis, YAxis, Bar  } from "recharts";
 
+const style = {
+  top: '50%',
+  right: 0,
+  transform: 'translate(0, -50%)',
+  lineHeight: '24px',
+};
 
 const vendorsYc = [
-  { label: 'TCM', id: 1 },
-  { label: 'YPLAY', id: 2},
-  { label: 'WSP', id: 3},
-  { label: 'HSL', id: 5},
   { label: 'Adylnet', id: 7},
-]
+  { label: 'Americanet', id: 16},
+  { label: 'D-Net', id: 13},
+  { label: 'HSL', id: 5},
+  { label: 'NOVA NET', id: 14},
+  { label: 'TCM', id: 1 },
+  { label: 'Videocast', id: 9},
+  { label: 'WSP', id: 3},
+  { label: 'youcast', id: 17},
+  { label: 'YPLAY', id: 2},
+];
+
+
+
+export const options = {
+  title: "My Daily Activities",
+  pieHole: 0.4,
+  is3D: false,
+};
+
+const COLORS = ['#364958', '#3b6064', '#55828b', '#87bba2', '#c9e4ca', ];
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 function Watching() {
 
     const [codigo, setCodigo] = useState('');
     const [ filterId, setFilterId] = useState('');
-    const [vendorNumber, setVendorNumber] = React.useState(null);
+    const [vendorNumber, setVendorNumber] = React.useState('');
 
     const [ watchingQtdChannels, setWatchingQtdChannels] = useState([ {
-      IDVendor: '',
       Vendor: '',
-      Canal: '',
-      URL: '',
-      QTD: '',
+      data: [{
+        Vendor: '',
+        IDVendor: '',
+        Canal: '',
+        URL: '',
+        QTD: '',
+      }],
     } ])
+
     const [ watchingQtdDevices, setWatchingQtdDevices] = useState([ {
-      IDVendor: '',
       Vendor: '',
-      Tipo: '',
-      Versao: '',
-      customers: '',
+      data: [{
+        IDVendor: '',
+        Vendor: '',
+        Tipo: '',
+        Versao: '',
+        customers: '',
+      }],
     } ])
 
     const handleChange = (event) => {
@@ -82,7 +122,7 @@ function Watching() {
       (async () => {
         const result = await api.get('monitoring/mw/customers/watching/qtd/channels')
         .then((result) => {
-          console.log("aq oe", result.data.response);
+          //console.log("aq oe", result.data.response);
           setWatchingQtdChannels(result.data.response);
   
     })
@@ -96,7 +136,7 @@ function Watching() {
       (async () => {
         const result = await api.get('monitoring/mw/customers/watching/qtd/devices')
         .then((result) => {
-          console.log("aq oe", result.data.response);
+          //console.log("aq oe", result.data.response);
           setWatchingQtdDevices(result.data.response);
   
     })
@@ -112,7 +152,6 @@ function Watching() {
 
 
     }, [])
-
     return(
         <div style={{width: '100%', height: '100%', display: 'flex',}}>
           <div style={{}}>
@@ -121,7 +160,7 @@ function Watching() {
           <div style={{ width: '100%', height: 2000}}>
             <Header />
 
-
+            {/*parte do input vendor */}
             <div style={{width: '95%', margin: 'auto',  borderRadius: 15}}>
               <Box sx={{ minWidth: 220 }}>
                 <FormControl sx={{ width: 180 }}>
@@ -129,12 +168,12 @@ function Watching() {
                   <Select
                     labelId="outlined-select-label"
                     id="outlined-select-currency"
-                    value={vendorNumber === null ? '' : vendorNumber}
+                    value={vendorNumber === '' ? '' : vendorNumber}
                     label="VendorNumber"
                     onChange={handleChange}
                   >
                   {vendorsYc.map((option, w) => (
-                  <MenuItem key={w} value={option.id}>
+                  <MenuItem key={w} value={option.label}>
                     {option.label}
                   </MenuItem>
                 ))}
@@ -142,60 +181,106 @@ function Watching() {
                 </FormControl>
               </Box>
 
-              {watchingQtdDevices.filter((item, index) => {
-              if(vendorNumber != null) {
-                if(item.IDVendor === vendorNumber) {
-                  return console.log("achou", );
-                } else {
-                  console.log("nada")
-                }
-              }
-            })}
-            <Container style={{height: 400, color: 'red'}}>
-              {watchingQtdDevices.filter((item) =>
-              vendorNumber != null && item.IDVendor === vendorNumber 
-              ).map((item, index) => {
+              {/*parte dos graficos */}
+              <Grid container component="main" sx={{  display: 'flex', }} spacing={2}>
 
-                console.log(item)
-                const date = [
 
-                ]
-                
-                return(
-                  <React.Fragment key={index}>
-                  <div >
-                  <PieChart width={500} height={200}>
-                    <Pie
-                      dataKey="customers"
-                      isAnimationActive={false}
-                      data={item}
-                      cx={200}
-                      cy={200}
-                      outerRadius={80}
-                      fill="#8884d8"
-                      label
-                    />
-                    <Tooltip />
-                  </PieChart>
-                  </div>
-                  </React.Fragment>
-                )
-              }
-              
-              
-              
-              )}
+                <Grid item xs={12} sm={12} md={6} elevation={4} square>
+                {watchingQtdDevices.filter((item) =>
+                  vendorNumber !== '' && item.Vendor === vendorNumber 
+                  ).map((item, index) => {
+                    const data = item.data.map((items) => (
+                      {
+                          "name": items.Tipo+" - "+items.Versao,
+                          "value": items.customers
+                      }
+                    ))
+
+                    return(
+                      <React.Fragment key={index}>
+                        <Container style={{width: '100%', height: 'auto', margin: 'auto', borderRadius: 10}}>
+                        <div style={{margin: 'auto', width: '100%', paddingTop: 1}}>
+                        <h3 style={{textAlign: 'center',}}>Usuários Assistindo por Dispositivo</h3>
+                        <PieChart width={470} height={280}>
+                        <Pie
+                          data={data}
+                          cx={120}
+                          cy={140}
+                          labelLine={false}
+                          label={renderCustomizedLabel}
+                          outerRadius={100}
+                          fill="#8884d8"
+                          dataKey="value"
+                        >
+                          {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                          ))}
+                        </Pie>
+                          <Tooltip />
+                          <Legend layout="vertical" verticalAlign="middle" wrapperStyle={style}/>
+                            </PieChart>
+                        </div>
+                        </Container>
+                      </React.Fragment>             
+                    )
+                  }      
+                  )}
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={6} elevation={4} square>
+                <Container style={{ height: 370, borderRadius: 10}}>
+
+                </Container>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} elevation={4} square>
+                  {watchingQtdChannels.filter((item) =>
+                  vendorNumber !== '' && item.Vendor === vendorNumber 
+                  ).map((item, index) => {
+                    const data = item.data.map((items) => (
+                      {
+                          "name": items.Canal,
+                          "Quantidade de usuários": items.QTD
+                      }
+                    ))
+
+                    return(
+                      <React.Fragment key={index}>
+                        <Container style={{width: '100%', height: 'auto', margin: 'auto', borderRadius: 10}}>
+                        <div style={{margin: 'auto', width: '100%', paddingTop: 1}}>
+                        <h3 style={{textAlign: 'center',}}>Usuários Assistindo por Canal</h3>
+                        <BarChart
+                          width={1200}
+                          height={400}
+                          data={data}
+                          margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5
+                          }}
+                        >
+                          <XAxis dataKey="name" />
+                          <YAxis />
+                          <Bar dataKey="Quantidade de usuários" fill="#55828b" />
+                          <Legend />
+                          <Tooltip />
+                        </BarChart>
+                        </div>
+                        </Container>
+                      </React.Fragment>             
+                    )
+                  }      
+                  )}
+
+                </Grid>
+
+
+              </Grid>
+
             
-            </Container>
-              <Container style={{height: 250, marginTop: 40}}>
-
-              </Container>
-              <Container style={{height: 450, marginTop: 40}}>
-              <SimpleChart style={{margin: 'auto'}} />
-              </Container>
 
             </div>
-
 
             <ThemeMenu/> 
           </div>
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://crm.youcast.tv.br/test/",
   headers: {
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiMSIsImlhdCI6MTY0ODU3NDU4NCwiZXhwIjoxNjQ4NTg1Mzg0fQ.kiZ7Y0155GmWGsTEQKT-TZVPmoNt2773I5cMmH2ETck',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiMSIsImlhdCI6MTY0ODc1MjcwMywiZXhwIjoxNjQ4NzYzNTAzfQ.tEBC_peL1g1e9zLq1FNhuhjr9yLE_bIYyyIw-v6qPnU',
   },
 
 });
